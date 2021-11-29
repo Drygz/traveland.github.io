119eb123ca37807eb777764c0ed7f96bf1b2089d
Vladimir <89993636+Drygz@users.noreply.github.com>
2021-11-29 22:50:10 +0300

    up

--- app/js/app.js
+++ app/js/app.js
@@ -3,64 +3,24 @@
 // require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
 
 document.addEventListener('DOMContentLoaded', () => {
-	
-	// slider
 
-	const images = [...document.querySelectorAll('.banner__slider')];
-	const left = document.querySelector('#left');
-	const right = document.querySelector('#right');
-	const number = document.querySelector('#number');
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
 
-	let el = 0;
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset = offset + 256;
+    if (offset > 768) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
 
-	function firstRender(params) {
-		images.forEach((el, i) => {
-			if (i === 0) {
-				el.classList.add('active')
-			}
-		})
-		number.textContent = el + 1
-	}
-	firstRender();
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    offset = offset - 256;
+    if (offset < 0) {
+        offset = 768;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
 
-	function changeSlide() {
-
-		images.forEach((el) => {
-			el.classList.remove('active')
-		})
-		images[el].classList.add('active')
-		number.textContent = el + 1
-	}
-
-	left.addEventListener('click', (event) => {
-		if (!event.isTrusted) return; //for webpack
-		console.log('left', el);
-		if (el === 0) {
-			el = images.length - 1
-		} else {
-			el = el - 1
-		}
-		changeSlide()
-	})
-
-	right.addEventListener('click', (event) => {
-		if (!event.isTrusted) return; //for webpack
-		console.log('right', el);
-		if (el === images.length - 1) el = 0
-		else el = el + 1
-		changeSlide()
-	})
-
-	// tab
-
-	(function(){
-		('.tab-nav li:first').addClass('select');
-		('.tab-panels>div').hide().filter(':first').show();
-		('.tab-nav a').mouseover(function(){ 
-		  ('.tab-panels>div').hide().filter(this.hash).show();
-		  ('.tab-nav li').removeClass('select'); 
-		  (this).parent().addClass('select');
-		  return (false);
-		})
-	  });
 })
